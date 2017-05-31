084e535296bee0cccb5d66d7e2ed532178d0b604
sushanth271 <mshushanth@gmail.com>
2017-05-31 11:20:24 +0530

    Create login_ethans.php

---
+++ login_ethans.php
@@ -0,0 +1,8 @@
+<?php
+$db_name  = "ethans";
+$mysql_username = "root";
+$mysql_password = "shushanth";
+$server_name = "localhost";
+$conn = mysqli_connect($server_name, $mysql_username, $mysql_password,$db_name);
+
+?>
